bc1f358d0a2761f7893986054bdff0eda4b22bd0
Trim Bresilla <trim.bresilla@gmail.com>
2018-03-04 08:19:50 +0100

    python + javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,66 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block{
     constructor(index, timestamp, data, previousHash = ""){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = "";
+        this.hash = this.calculateHash();
+        this.nonce = 0
     }
 
     calculateHash(){
-        
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block minned " + this.hash)
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/01/2018", "genesis block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length -1]
+    }
+
+    addNewBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty)
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for (let i = 1; i < this.chain.length; i++) {
+                const currentBlock = this.chain[i]
+                const previousBlock = this.chain[i-1]
+                if (currentBlock.previousHash !== previousBlock.hash){
+                    return false;
+                }
+        }
+        return true;
+    }
+}
+
+
+let dardan = new Blockchain;
+
+console.log("Mining block ...")
+dardan.addNewBlock(new Block(1, "02/01/2018", {amount: 55}));
+console.log("Mining block ...")
+dardan.addNewBlock(new Block(2, "02/01/2018", {amount: 25}));
+console.log("Mining block ...")
+dardan.addNewBlock(new Block(3, "03/01/2018", {amount: 15}));
--- python/blockchain.py
+++ python/blockchain.py
@@ -1,50 +1,63 @@
-import hashlib as hasher
-import datetime as date
+import hashlib, json, logging 
+from datetime import datetime 
+
 
 
 class Block:
-  def __init__(self, index, timestamp, data, previous_hash):
-    self.index = index
-    self.timestamp = timestamp
-    self.data = data
-    self.previous_hash = previous_hash
-    self.hash = self.hash_block()
-  
-  def hash_block(self):
-    sha = hasher.sha256()
-    sha.update(str(self.index) + 
-               str(self.timestamp) + 
-               str(self.data) + 
-               str(self.previous_hash))
-    return sha.hexdigest()
+	def __init__(self, index, timestamp, data, prevHash = ""):
+		self.index = index
+		self.timestamp = timestamp
+		self.data = data
+		self.prevHash = prevHash
+		self.nonce = 0
+		self.hash = self.hashBlock()
+	
+	def hashBlock(self):
+		sha = hashlib.sha256()
+		sha.update(str(self.index).encode("utf-8") + 
+				str(self.timestamp).encode("utf-8") + 
+				str(self.data).encode("utf-8") + 
+				str(self.prevHash).encode("utf-8") +
+				str(self.nonce).encode("utf-8"))  
+		return sha.hexdigest()
+
+	def mineBlock(self, difficulty):
+		while self.hash[:difficulty] != "".join(map(str,[0 for i in range(difficulty)])):
+			self.nonce += 1 
+			self.hash = self.hashBlock()
+		print("BLOCK MINED")
 
 
+class Blockchain:
+	def __init__(self):
+		self.chain = [self.genesisBlock()]
+		self.difficulty = 4
 
-def create_genesis_block():
-  return Block(0, date.datetime.now(), "Genesis Block", "0")
+	def genesisBlock(self):
+		return Block(0, datetime.now(), "GENESIS", "0")
 
+	def latestBlock(self):
+		return self.chain[-1]
 
-def next_block(last_block):
-  this_index = last_block.index + 1
-  this_timestamp = date.datetime.now()
-  this_data = "Hey! I'm block " + str(this_index)
-  this_hash = last_block.hash
-  return Block(this_index, this_timestamp, this_data, this_hash)
+	def newBlock(self, newBlock):
+		newBlock.prevHash = self.latestBlock().hash
+		newBlock.mineBlock(self.difficulty)
 
+		print(newBlock.prevHash)
+		print(newBlock.hash)
 
-# Create the blockchain and add the genesis block
-blockchain = [create_genesis_block()]
-previous_block = blockchain[0]
+		self.chain.append(newBlock)
 
-# How many blocks should we add to the chain
-# after the genesis block
-num_of_blocks_to_add = 20
+	def checkValidity(self, previousBlock, currentBlock):
+		if previousBlock.hash != currentBlock.prevHash:
+			return False
+		else:
+			return True
+dardan = Blockchain()
 
-# Add blocks to the chain
-for i in range(0, num_of_blocks_to_add):
-  block_to_add = next_block(previous_block)
-  blockchain.append(block_to_add)
-  previous_block = block_to_add
-  # Tell everyone about it!
-  print "Block #{} has been added to the blockchain!".format(block_to_add.index)
-  print "Hash: {}\n".format(block_to_add.hash)
+print("MINING BLOCK ...")
+dardan.newBlock(Block(1, datetime.now(), "55"))
+print("MINING BLOCK ...")
+dardan.newBlock(Block(1, datetime.now(), "5"))
+print("MINING BLOCK ...")
+dardan.newBlock(Block(1, datetime.now(), "15"))
