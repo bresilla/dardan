8e97965f3cce698fc130abac2e1fae3d0225ebdb
Trim Bresilla <trim.bresilla@gmail.com>
2019-07-18 12:09:33 +0000

    only CPP going forward

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bresilla/WORK/tull
-# Build directory: /home/bresilla/WORK/tull/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
-subdirs("thrd")
